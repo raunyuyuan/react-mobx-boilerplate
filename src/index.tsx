bf411e7850cb6e74ef0aabb81cfff17796973f38
ruanyuyuan <765426599@qq.com>
2020-03-26 21:29:42 +0800

    eject config and add less

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './pages/App';
+import App from '@/pages/App';
 import * as serviceWorker from './serviceWorker';
-import './assets/styles/global.scss';
+import '@/assets/styles/global.less';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
